a6138a7990fb9653d07ee4937099cb9cf51248ed
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-26 10:18:00 +0000

    variables

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "zaifpasha@gmail"
+var accountPassword = 1445
+accountCity = "Jaipur"
+
+// accountId = 23
+// console.log(accountId)
+
+accountEmail ="alipasha@gmail.com"
+accountPassword = "234"
+accountCity="Mumbai"
+
+console.table([accountId , accountEmail , accountPassword, accountCity])

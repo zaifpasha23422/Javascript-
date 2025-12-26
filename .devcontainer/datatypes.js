6edac8245241d9d121d7fa275d18cd528386dd28
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-26 10:35:23 +0000

    datatypes

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,23 @@
+// "use strict" ; treat all JS code as newer version 
+// alert("zaif") we are using node js , not browser
+
+
+// Datatypes 
+
+// Primititve 
+// number 
+// null 
+// string 
+// symbol 
+// boolean
+// bigint 
+// undefined 
+
+// Non Primitive 
+// object 
+
+
+console.log(typeof "zaif")
+
+console.log(null)
+console.log(undefined)

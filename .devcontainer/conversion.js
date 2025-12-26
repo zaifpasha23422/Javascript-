19312be37757b64b18b85b4adb80869305432eec
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-26 13:02:35 +0000

    conversion

---
+++ .devcontainer/conversion.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+// let mark = 44
+
+// console.log(typeof score );
+// console.log(typeof ((mark)));
+// console.log(typeof ((mark)));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// // "33" = 33 
+// "33abc" = Nan
+// true = 1 , false = 0
+
+
+// let isLoggedIn = "zaif"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn); 
+
+// 1 = true 
+// 0 = false
+// "zaif" = true 
+// "" = false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); 
+
+console.log(typeof stringNumber);

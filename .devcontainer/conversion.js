01a8e9db27d21ea5154b34061d1b99c5dd2b0e6c
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-26 13:21:44 +0000

    conversion

--- .devcontainer/conversion.js
+++ .devcontainer/conversion.js
@@ -25,9 +25,30 @@ let score = "33abc"
 // "zaif" = true 
 // "" = false
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber); 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); 
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// let value = 3 
+// let negvalue = -value
+// console.log(negvalue); 
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(3%2);
+
+// let str1 = "hello"
+// let str2 = "zaif"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2"); 

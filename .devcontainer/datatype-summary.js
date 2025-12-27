be8516aad05366c79ba4fb065ebef3c94ef82159
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-27 07:05:24 +0000

    datatype summary

---
+++ .devcontainer/datatype-summary.js
@@ -0,0 +1,35 @@
+// Datatype 
+
+// Primitive 
+// Number String Booelean Null undefined BigInt Symbol
+
+// Non Primitive TypeError
+// Array Object Function 
+
+// const score = 100
+// const scorevalue = 100.3 
+// const isLoggedIn = false
+
+// const name = "zaif"
+
+// const outsideTemp = null
+
+// let userEmail;  we can declare but not assign a value 
+
+// const bigNumber = 12345343443445399329829n
+// console.log(bigNumber)
+
+
+// NOn primitive (reference type)
+
+// let hero =["spiderman" , "batman" , "ironman"];
+
+// let object= {
+//     name: "zaif",
+//     age:22
+// };
+
+const myFunction = function (){
+    console.log("hello world")
+}
+console.log(typeof myFunction)

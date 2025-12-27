9ad3cd8da409169ac60edf0a1c362947292a82c4
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-27 12:22:17 +0000

    maths

---
+++ .devcontainer/numbers_and_maths.js
@@ -0,0 +1,39 @@
+// const score = 400 
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 123.78237
+// console.log(otherNumber.toPrecision(3));
+
+//  const hundreds =1000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+
+// Maths 
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(2, 3, 5 ,7 ));
+// console.log(Math.max(8, 9, 2, 7));
+
+// console.log(Math.random()); it gives a value between 0 to 1 
+
+
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1) to avoid 0 we add 1
+// console.log(Math.floor(Math.random()*10) + 1)
+
+
+const min= 10 
+const max= 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)))
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)

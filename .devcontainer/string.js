ed599f6342cc55d85f45a310f0a57dc73f2c5ebd
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-27 10:45:24 +0000

    string methods

---
+++ .devcontainer/string.js
@@ -0,0 +1,24 @@
+// const name = "zaif"
+// const repoCount = 50
+
+// console.log(name + repoCount + "value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('zaifpasha')
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+
+// const newStringOne = "   zaif"
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://zaif.com/ali.com/20%"
+console.log(url.replace('zaif', 'amash'))
+console.log(url.includes('amash'));
+

1aef9151d459a13b27519690d93ef1a3f3105215
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-29 07:08:31 +0000

    date

---
+++ .devcontainer/date.js
@@ -0,0 +1,35 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+
+// let createDate = new Date(2024,10,17, 5 ,53)
+// console.log(createDate.toLocaleString());
+
+// let date = new Date("2025-01-10")
+// console.log(date.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+// console.log(Date.now());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+

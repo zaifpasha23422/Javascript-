88d1a0c44712c926862d4a6fa553fb958737a75c
zaifpasha23422 <zaifpasha23422@gmail.com>
2025-12-29 08:36:21 +0000

    array second

---
+++ .devcontainer/array_2.js
@@ -0,0 +1,27 @@
+const bollywood_hero = ["salman" , "sharukh", "amir", "akshay"]
+const bollywood_heroine = ["katrina", "kareena", "kiara", "alia" ]
+const south_hero = ["ramcharan", "prabhas", "mahesh"]
+// bollywood_hero.push(bollywood_heroine);
+// console.log(bollywood_hero);
+
+// concate operator
+// const all_celebs= bollywood_hero.concat(bollywood_heroine);
+// console.log(all_celebs);
+
+// spread operator
+// const all_celeb = [...bollywood_hero, ...bollywood_heroine, ...south_hero]
+// console.log(all_celeb);
+
+// const another_array =[1,2,3,[4,7,8], 3,5,[7,9,9,2],3 ,1]
+// const real_another_array =[another_array.flat(Infinity)]
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Zaifpasha"));
+// console.log(Array.from("Zaifpasha"));
+// console.log(Array.from({name: "zaif"})); it gives a empty value  
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3)); 

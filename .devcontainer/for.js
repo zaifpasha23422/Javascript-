f9b00fe9c1ee6b1ec3901df68400223b79c6160c
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-01-07 11:49:50 +0000

    for loop

---
+++ .devcontainer/for.js
@@ -0,0 +1,72 @@
+// for
+
+// for(let i = 0; i<=10; i++) {
+//     console.log("zaif");
+// }
+
+
+// for(let i=1; i<=10; i++){
+//     const element  = i;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//      for (let j = 0; j <=5; j++) {
+//         console.log(`Inner loop value ${j} and inner loop value ${i}`); 
+//      }
+//     }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//      for (let j = 1; j <=10; j++) {
+//         console.log(i + '*' + j +'='+ i*j)
+//      }
+//     }   
+    
+
+// const num = 2    
+// for(i=1 ; i<=10 ; i++){
+//     console.log(num + '*' + i + '=' + num*i );
+// }    
+
+// const num =7
+// for(i=1; i<=10; i++){
+//     console.log(`${num} * ${i} = ${num*i}`)
+// }
+
+// for(i=1; i<=10; i++){
+//     if(i%2 !==0){
+//         console.log(i)    
+//     }
+   
+// }
+
+// let myArray = ["spiderman", "batman", "ironman" , "flash", "superman"]
+// console.log(myArray.length);
+// for(let i=0; i<myArray.length; i++){
+//     const element = myArray[i]
+//     console.log(element);
+// }
+
+//  for(let i=1; i<=20; i++){
+//     if(i==5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${i}`)
+//  }
+
+//   for(let i=1; i<=20; i++){
+//     if(i==5){
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`Value of i is ${i}`)
+//  }
+

ddbd3e56108fd31867220098e7f8870213e7db75
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-01-07 12:40:53 +0000

    while

---
+++ .devcontainer/whilee.js
@@ -0,0 +1,21 @@
+
+// let index = 1
+// while (index <=10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+let myArray = ["flash" , "ironman" , "spiderman" , "superman"]
+let arr = 0
+while (arr < myArray.length){  
+    arr = arr +1
+}
+ console.log(myArray[1]); 
+
+// let score =11
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+// }
+// while(score <=10);

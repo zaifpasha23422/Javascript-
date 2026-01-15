ba87843a0b8f20b9250fd7522342c6fc455a5a9f
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-01-15 11:30:46 +0000

    Dom fourth

---
+++ .devcontainer/project/four.js
@@ -0,0 +1,14 @@
+const randomNumber = parseInt(Math.random()*100 + 1);
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('lowOrHi')
+
+const startOver = document.querySelector('.resultParas')
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1 
+let playGame =true

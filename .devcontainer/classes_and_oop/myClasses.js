9b4bdf4996bd07bf082a51ca879cc6780ee43438
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-01-22 12:10:50 +0000

    classes

---
+++ .devcontainer/classes_and_oop/myClasses.js
@@ -0,0 +1,31 @@
+// class User {
+//     constructor(username, email, password){
+//     this.username =username
+//     this.email =email
+//     this.password = password
+// }
+// encryptPassword(){
+//     return `${this.password}abc`
+// }
+// changeUserName(){
+//     return `${this.username.toUpperCase()}`
+// }
+// }
+// const chai = new User("chai", "chai@gmail","123")
+// console.log(chai.encryptPassword())
+// console.log(chai.changeUserName())
+
+function User(username, email, password){
+    this.username = username,
+    this.email = email,
+    this.password = password 
+}
+User.prototype.encryptPassword = function (){
+    return `${this.password}abc`
+}
+User.prototype.changeUserName = function (){
+    return `${this.username.toUpperCase()}`
+}
+const tea = new User("tea" , "tea@gmail.com", "123")
+console.log(tea.encryptPassword())
+console.log(tea.changeUserName())

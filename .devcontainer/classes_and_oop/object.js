f812325769440a634e9ae02293962efee43039dc
zaifpasha23422 <zaifpasha23422@gmail.com>
2026-01-22 08:20:58 +0000

    object

---
+++ .devcontainer/classes_and_oop/object.js
@@ -0,0 +1,27 @@
+// function multiplyby5 (num){
+//     return num*5
+// }
+// multiplyby5.power = 2
+// console.log(multiplyby5(5));
+// console.log(multiplyby5.power);
+// console.log(multiplyby5.prototype);
+
+// function createUser(username, prize){
+//     this.username=username
+//     this.prize=prize
+// }
+// createUser.prototype.increment = function(){
+//     this.prize = prize
+// }
+// createUser.prototype.printMe = function(){
+//     console.log(`score is ${this.prize}`)
+// }
+// createUser.prototype.name = function(){
+//     console.log(`i want a ${this.username}`)
+// }
+// const chai = new createUser("chai",25)
+// const tea = new createUser("tea",250)
+
+
+// chai.printMe()
+// tea.name()
